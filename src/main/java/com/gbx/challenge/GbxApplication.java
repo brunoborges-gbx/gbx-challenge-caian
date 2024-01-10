dc07c1755293e75947d842be46dfdd1613d7d2fd
Caian Marcinkowski <caianferre@gmail.com>
2024-01-10 08:55:22 -0300

    first commit

---
+++ src/main/java/com/gbx/challenge/GbxApplication.java
@@ -0,0 +1,13 @@
+package com.gbx.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbx/challenge/GbxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbx.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
